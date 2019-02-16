329477049bba739ec286c2df2fd83b0e8aae823d
amuroni <muronialessandro@gmail.com>
2019-02-16 16:14:14 +0100

    Working with classes

--- ClassCreation_exercises.py
+++ ClassCreation_exercises.py
@@ -1,35 +1,35 @@
-class Worker:
-    def __init__(self, name, pay):  # defining the main class attributes, self is the subject of the class!
-        self.name = name  # attribute aka "state information"
-        self.pay = pay  # attribute aka "state information"
-
-    def lastname(self):
-        return self.name.split()[-1]  # splits the string on blanks!
-
-    def giveraise(self, percent):
-        self.pay *= (1.0 + percent)  # update on pay raise
-
-
-bob = Worker('Bob Smith', 50000)  # Make two instances
-sue = Worker('Sue Jones', 60000)  # Each has name and pay attributes
-
-print(bob.lastname())  # call the method were "bob" is "self"
-print(sue.lastname())
-print(str(sue.pay) + " € before the raise")
-sue.giveraise(.10)
-print(str(round(sue.pay)) + " € after the raise, nice!") # rounded the number too
-print("***********")
-
-
-class rec: pass
-
-
-pers1 = rec()
-pers1.name = "Bob"
-pers1.job = ["dev", "manager"]
-pers1.age = 30
-
-print("My name is", pers1.name + ", I'm a", str(pers1.age) + " years old", pers1.job[0])
+# class Worker:
+#     def __init__(self, name, pay):  # defining the main class attributes, self is the subject of the class!
+#         self.name = name  # attribute aka "state information"
+#         self.pay = pay  # attribute aka "state information"
+#
+#     def lastname(self):
+#         return self.name.split()[-1]  # splits the string on blanks!
+#
+#     def giveraise(self, percent):
+#         self.pay *= (1.0 + percent)  # update on pay raise
+#
+#
+# bob = Worker('Bob Smith', 50000)  # Make two instances
+# sue = Worker('Sue Jones', 60000)  # Each has name and pay attributes
+#
+# print(bob.lastname())  # call the method were "bob" is "self"
+# print(sue.lastname())
+# print(str(sue.pay) + " € before the raise")
+# sue.giveraise(.10)
+# print(str(round(sue.pay)) + " € after the raise, nice!") # rounded the number too
+# print("***********")
+#
+#
+# class rec: pass  # class with no defs inside, assigned below
+#
+#
+# pers1 = rec()
+# pers1.name = "Bob"
+# pers1.job = ["dev", "manager"]
+# pers1.age = 30
+#
+# print("My name is", pers1.name + ", I'm a", str(pers1.age) + " years old", pers1.job[0])
 
 
 class Person:
@@ -46,26 +46,54 @@ class Person:
         self.pay = int(self.pay * (1 + percent))
 
     def __repr__(self):
-        return "[Person: %s %s, Salary: %s Euro]" % (self.gender, self.name, self.pay)  # method and print + % format
+        return "[Person: %s %s, Job: %s, Salary: %s Euro]" % (self.gender, self.name, self.job, self.pay)  # method and print + % format
 
 
-class Manager(Person):  # this one is a subclass of person! inherits person attrs
+class Manager:  # this one is a subclass of person! inherits person attrs; uses class delegation
     def __init__(self, gender, name, pay):
-        Person.__init__(self, gender, name, "mgr", pay)  # job =mgr by subclass default
+        self.person = Person(gender, name, "mgr", pay)  # job =mgr by subclass default + embed person obj
 
     def giveraise(self, percent, bonus=.10):
-        Person.giveraise(self, percent + bonus)  # augmented the original giveraise def
+        self.person.giveraise(percent + bonus)  # intercept person giveraise, use percent + bonus
+
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)       # delegate other attributes
+
+    def __repr__(self):
+        return str(self.person)
+
+
+class Department:  # = aggregating embedded objs into a composite obj
+    def __init__(self, *args):
+        self.members = list(args)
+
+    def addmember(self, person):
+        self.members.append(person)
+
+    def giveraises(self, percent):
+        for person in self.members:
+            person.giveraise(percent)
+
+    def showall(self):
+        for person in self.members:
+            print(person)
 
 
 if __name__ == "__main__":
+    bob = Person("Mr.", "Bob Jones", "dev", 20000)
     tom = Manager("Mr.", "Tom Malone", 50000)  # no need to specify job anymore
     may = Person("Ms.", "May Jones", job="dev", pay=100000)
-    print("All persons")
-    for obj in (tom, may):
-        obj.giveraise(.10)
-        print(obj)
+    # print("All persons")
+    # for obj in (tom, may):
+    #     obj.giveraise(.10)
+    #     print(obj)
 
-# instead of using the code below, we process all obj at one
+development = Department(bob, may)  # development = name of aggregate obj Department
+development.addmember(tom)
+development.giveraises(.10)
+development.showall()
+
+# instead of using the code below, we process all objs at one
 # print("My name is", may.name, "and my pay is", may.pay, "Euro")
 # print(may.lastname())
 # # may.giveraise(.30)
