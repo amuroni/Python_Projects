147daf2b9a45f82fc26b3d3b2a3f76e6d09ba05f
Alessandro Muroni <muronialessandro@gmail.com>
2019-02-27 14:20:41 +0100

    Class Coding basics exercises

--- ClassCodingBasics.py
+++ ClassCodingBasics.py
@@ -350,3 +350,49 @@ print(list(map(lambda act: act(5), actions)))  # [25, 7, 15]
 actions = [square, s_obj, p_obj.method, Negate]  # also with Negate class
 for act in actions:
     print(act(2))
+
+
+class ListInstance:  # overloads str method
+    def __attrnames(self):
+        result = ""
+        for attr in sorted(self.__dict__):
+            result += "\t%s=%s\n" % (attr, self.__dict__[attr])
+        return result
+
+    def __str__(self):
+        return "<Instance of %s, address %s:\n%s>" % (
+                                    self.__class__.__name__,
+                                    id(self),
+                                    self.__attrnames())
+
+#
+# class Spam(ListInstance):
+#     def __init__(self):
+#         self.data1 = "food"
+#
+#
+# x = Spam()
+# print(x)
+
+
+class Super:
+    def __init__(self):
+        self.data1 = "Spam"
+
+    def ham(self):
+        pass
+
+
+class Sub:
+    def __init__(self):
+        Super.__init__(self)
+        self.data2 = "Eggs"
+        self.data3 = 42
+
+    def spam(self):
+        pass
+
+
+if __name__ == "__main__":
+    X = Sub()
+    print(X)
