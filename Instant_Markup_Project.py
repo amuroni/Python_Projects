626668859e2063ba191915ca7acc61325591313f
Alessandro Muroni <muronialessandro@gmail.com>
2019-03-02 10:05:01 +0100

    Advanced class programming

---
+++ Instant_Markup_Project.py
@@ -0,0 +1,41 @@
+"""This app will change a plain text file content into a marked up language such as HTML.
+It needs to classify various text elements and clearly mark them
+"""
+
+import sys
+import re
+
+
+# collect lines
+def lines(file):
+    for line in file: yield line
+    yield "\n"
+
+
+# collect lines until you find an empty line, then call it a block
+def blocks(file):
+    block = []
+    for line in lines(file):
+        if line.strip():
+            block.append(line)
+        elif block:
+            yield "".join(block).strip()
+            block = []
+
+
+print('<html><head><title>...</title><body>')
+
+title = True
+for block in blocks(sys.stdin):
+    block = re.sub(r'\*(.+?)*', r'<em>\1</em>', block)
+    if title:
+        print("<h1>")
+        print(block)
+        print("</h1>")
+        title = False
+    else:
+        print("<p>")
+        print(block)
+        print("</p>")
+
+print('</body></html>')
