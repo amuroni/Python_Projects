7db683ed65d4a47964294e6ea21c8ada3ac76998
Alessandro Muroni <muronialessandro@gmail.com>
2019-03-07 19:53:25 +0100

    Data visualization

--- Data Visualization/rw_visual.py
+++ Data Visualization/rw_visual.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk(100000)
-rw.fill_walk()
-plt.scatter(0, 0, c="blue", s=100)
-plt.scatter(rw.x_values, rw.y_values, c="green", edgecolors="none", s=1)
-plt.axes().get_xaxis().set_visible(False)
-plt.axes().get_yaxis().set_visible(False)
-plt.show()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.figure(dpi=128, figsize=(10, 6))
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(0, 0, c="green", edgecolors="none", s=100)  # starting point, marked green
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100) # last point, marked red
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=1)
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+    plt.show()
+
+    keep_running = input("Make another walk? y/n")
+    if keep_running == "n":
+        break
